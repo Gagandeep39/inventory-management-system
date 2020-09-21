989c67dc6dbe1efb9da4cfff2be428ff19c23e9f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-21 09:12:03 +0530

    Added mapper methods

--- inventory-auth-service/src/main/java/com/cg/inventoryauthservice/helper/UserDetailsMapper.java
+++ inventory-auth-service/src/main/java/com/cg/inventoryauthservice/helper/UserDetailsMapper.java
@@ -3,11 +3,13 @@
  * @email singh.gagandeep3911@gmail.com
  * @create date 2020-09-20 23:32:45
  * @modify date 2020-09-20 23:32:45
- * @desc [description]
+ * @desc Mapps Entities with Dto
  */
 package com.cg.inventoryauthservice.helper;
 
 import com.cg.inventoryauthservice.dto.RegisterRequest;
+import com.cg.inventoryauthservice.dto.UserDetailsDto;
+import com.cg.inventoryauthservice.entity.Address;
 import com.cg.inventoryauthservice.entity.User;
 import com.cg.inventoryauthservice.entity.UserDetails;
 import com.cg.inventoryauthservice.enums.Gender;
@@ -27,6 +29,34 @@ public class UserDetailsMapper {
       .dob(registerRequest.getDob())
       .emailId(registerRequest.getEmailId())
       .phoneNo(registerRequest.getPhoneNo())
+      .build();
+  }
+
+  public static UserDetailsDto userDetailsToDto(UserDetails userDetails){
+    return UserDetailsDto.builder()
+      .userId(userDetails.getUserDetailsId())
+      .username(userDetails.getUser().getUsername())
+      .role(userDetails.getUser().getRole())
+      .phoneNo(userDetails.getPhoneNo())
+      .gender(userDetails.getGender().toString())
+      .designation(userDetails.getDesignation())
+      .dob(userDetails.getDob())
+      .emailId(userDetails.getEmailId())
+      .address(userDetails.getAddress())
+      .build();
+  }
+
+  public static UserDetails dtoToUserDetails(UserDetailsDto userDetailsDto) {
+    Address address = userDetailsDto.getAddress();
+    address.setAddressId(userDetailsDto.getUserId());
+    return UserDetails.builder()
+      .address(address)
+      .userDetailsId(userDetailsDto.getUserId())
+      .phoneNo(userDetailsDto.getPhoneNo())
+      .gender(Gender.valueOf(userDetailsDto.getGender()))
+      .designation(userDetailsDto.getDesignation())
+      .dob(userDetailsDto.getDob())
+      .emailId(userDetailsDto.getEmailId())
       .build();
   }
   
