9a8f3150f2e17aa9cf7e202cfa37f468e84b5183
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-25 22:48:03 +0530

    Added discovery server support

--- inventory-auth-service/src/main/java/com/cg/inventoryauthservice/InventoryAuthServiceApplication.java
+++ inventory-auth-service/src/main/java/com/cg/inventoryauthservice/InventoryAuthServiceApplication.java
@@ -1,8 +1,17 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-09-25 22:45:07
+ * @modify date 2020-09-25 22:45:07
+ * @desc Starting point
+ */
 package com.cg.inventoryauthservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class InventoryAuthServiceApplication {
 
